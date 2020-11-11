559e458b6ea12590768d2b624970dd5bfa28ca66
Abhishek Angira <abhishek@angira.net>
2020-11-11 22:59:18 +0530

    fuckk firebase

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,9 @@
-let header = document.getElementsByClassName('header')[0];
+let header = document.getElementsByClassName("header")[0];
 
-console.log("yolo MFer")
+console.log("yolo MFer");
+
+// let functions = firebase.functions();
+
+let mylog = firebase.functions().httpsCallable("log");
+
+console.log(mylog("hi"));
