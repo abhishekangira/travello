1058a7119955ad1dd8a7173c7c73b6a9ad082f71
Abhishek Angira <abhishek@angira.net>
2020-11-12 00:27:27 +0530

    update func

--- api/func.js
+++ api/func.js
@@ -1,7 +1,7 @@
-export default (req, res) => {
+module.exports = (req, res) => {
     res.json({
-      body: req.body,
-      query: req.query,
-      cookies: req.cookies,
-    })
-  }
+        body: req.body,
+        query: req.query,
+        cookies: req.cookies,
+    });
+};
