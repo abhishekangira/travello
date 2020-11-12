1d8e1090434da82db428e211f342fafe88b66c57
Abhishek Angira <abhishek@angira.net>
2020-11-12 15:50:07 +0530

    HTML Touchup

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,3 @@
 let header = document.getElementsByClassName("header")[0];
 
-console.log("yolo MFer");
-
-// let functions = firebase.functions();
-
-let mylog = firebase.functions().httpsCallable("log");
-
-console.log(mylog("hi"));
+// console.log(header__logo);
